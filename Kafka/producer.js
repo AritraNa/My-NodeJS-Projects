43b7f8731dedf5a500254ca0cb715a2350f8bab3
Aritra-N <aritra.naharay@k12onlineschools.com>
2024-11-19 09:38:59 +0530

    kafka producer addition

--- Kafka/producer.js
+++ Kafka/producer.js
@@ -1,11 +1,24 @@
 const { kafka } = require("./client");
+const readline = require("readline");
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
 async function init(params) {
     const producer = kafka.producer();
 
     console.log("connection producer...")
     await producer.connect();
     console.log("producer connected successfully");
+
+    rl.setPrompt('> ')
+    rl.prompt()
+
+    rl.on('line', async function (line) {
+        const [riderName , location] = line.split(' ')
+    })
+
 
     await producer.send({
         topic: "rider-updates",
